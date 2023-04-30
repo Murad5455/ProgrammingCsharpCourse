03a7a2a8e0088da79f780f747ad1d9b241b16832
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2023-04-30 12:13:59 +0400

    ders1

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //28Aprel
+                Console.WriteLine("Birinci ededi girin");
+                int var1;
+            while (true)
+            {
+                try
+                {
+                    var1 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Yalniz reqem girin");
+
+
+                }
+            }
+
+
+
+                Console.WriteLine("ikinci ededi girin");
+                int var2 = int.Parse(Console.ReadLine());
+                int sum = Sum(var1, var2);
+                Console.WriteLine("Cavab: " + sum);
+            
+        }
+            public static int Sum(int var1, int var2)
+            {
+                return (var1 + var2);
+            }
+
+        
+    }
+}
