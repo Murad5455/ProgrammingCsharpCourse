2877459baedb024aaeb49d4698fa35c377cc0727
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2023-05-03 12:27:50 +0400

    ders1-in ardi

--- Program.cs
+++ Program.cs
@@ -4,40 +4,64 @@ namespace Course
 {
     class Program
     {
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //    //28Aprel
+        //    Console.WriteLine("Birinci ededi girin");
+        //    int var1;
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            var1 = int.Parse(Console.ReadLine());
+        //            break;
+        //        }
+        //        catch (Exception)
+        //        {
+        //            Console.WriteLine("Yalniz reqem girin");
+
+
+        //        }
+        //    }
+
+
+
+        //    Console.WriteLine("ikinci ededi girin");
+        //    int var2 = int.Parse(Console.ReadLine());
+        //    int sum = Sum(var1, var2);
+        //    Console.WriteLine("Cavab: " + sum);
+        //}
+
+        //    public static int Sum(int var1, int var2)
+        //    {
+        //        return (var1 + var2);
+        //    }
+
         static void Main(string[] args)
-        {
+        {//inplicit
+            //int a = 45;
+            //long b = a;
 
-            //28Aprel
-            Console.WriteLine("Birinci ededi girin");
-            int var1;
-            while (true)
-            {
-                try
-                {
-                    var1 = int.Parse(Console.ReadLine());
-                    break;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Yalniz reqem girin");
+            //expilicit
+            //int var1 = 62;
+            //short var2 = (short)var1;
+            //Console.WriteLine(var2);
 
 
-                }
-            }
+            //int a = int.Parse(Console.ReadLine());
+            //int b = Convert.ToInt16(Console.ReadLine());
+            //if(a/2 && b/2)
+            //{ Console.WriteLine(true); }
+            //else { Console.WriteLine(false); }
 
-
-
-            Console.WriteLine("ikinci ededi girin");
-            int var2 = int.Parse(Console.ReadLine());
-            int sum = Sum(var1, var2);
-            Console.WriteLine("Cavab: " + sum);
         }
 
-            public static int Sum(int var1, int var2)
-            {
-                return (var1 + var2);
-            }
 
-        
+
+
     }
 }
